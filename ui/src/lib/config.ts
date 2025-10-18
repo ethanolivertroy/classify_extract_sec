05f2d6845e3089137a8473a9072a18883d254623
Jerry Liu <jerryjliu98@gmail.com>
2025-10-18 11:41:24 -0700

    Initial commit

---
+++ ui/src/lib/config.ts
@@ -0,0 +1,3 @@
+export const APP_TITLE = "Extraction Review Tmp5 Classify Sec";
+export const EXTRACTED_DATA_COLLECTION = "extraction-review-tmp5-classify-sec";
+export const AGENT_NAME = import.meta.env.VITE_LLAMA_DEPLOY_DEPLOYMENT_NAME;
